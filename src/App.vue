607fe1ee56bb29a090e27bb2a89306bdc0a67b54
pedro <pedropereda@hotmail.com>
2024-10-26 14:27:28 +0200

    Añadimos el código de Vue3

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>hola</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+console.log('hola')
+if (true) {
+  console.log('hola 2')
+}
+</script>
+
+<style scoped></style>
