a1c220b3c6ed6841f3253eec5647610e698a7d3e
pedro <pedropereda@hotmail.com>
2024-10-26 14:41:14 +0200

    Se añaden las modificaciones a prettier

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,17 @@
 </template>
 
 <script setup lang="ts">
+const objeto = {
+  valor: 1,
+  valor2: 3,
+  valor4: 5,
+  valor6: 7,
+  valor8: 9,
+  valor10: 11,
+  valor13: 14,
+  valor15: 12,
+  valor16: '12',
+}
 console.log('hola')
 if (true) {
   console.log('hola 2')
